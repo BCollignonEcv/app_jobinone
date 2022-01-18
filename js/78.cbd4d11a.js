fa2b98e6dd4edb2b612d448cecf0b88ed5c3ff40
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2022-01-18 19:36:30 +0100

    deploy

---
+++ js/78.cbd4d11a.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkapp_jobinone"]=self["webpackChunkapp_jobinone"]||[]).push([[78],{78:function(e,n,t){t.r(n),t.d(n,{startStatusTap:function(){return r}});var o=t(65),s=t(587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const r=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,o.wj)((()=>{const n=e.innerWidth,t=e.innerHeight,r=document.elementFromPoint(n/2,t/2);if(!r)return;const i=r.closest("ion-content");i&&new Promise((e=>(0,s.c)(i,e))).then((()=>{(0,o.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await i.scrollToTop(300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.cbd4d11a.js.map
